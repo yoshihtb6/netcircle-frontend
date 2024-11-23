936ce1bb01100fe804a282cd3c0fcb7791ec669f
yoshihtb6 <yoshiyoshi23.htb6@gmail.com>
2024-11-23 23:36:36 +0900

    shadcnの追加

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,16 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
-  const onClick = () => {
-    console.log('testtest')
+  const tmp = () => {
+    console.log("tmp");
   }
 
   return (
-    <>
-      <div>
-        <button onClick={onClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Buttonaaa
-        </button>
-      </div>
-    </>
-  )
+    <div>
+      <Button onClick={tmp}>Click me</Button>
+    </div>
+  )  
 }
 
 export default App
