2c48fd3b60f72d925b078cca1fa07ed14a42dddd
yoshihtb6 <yoshiyoshi23.htb6@gmail.com>
2024-12-13 21:36:32 +0900

    背景画像のファイル名を変更

--- app/src/pages/auth/LoginFormLayout.tsx
+++ app/src/pages/auth/LoginFormLayout.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "./LoginForm"
 
 export const LoginFormLayout = () => {
   return (
-    <section className="h-screen w-full bg-cover bg-center bg-no-repeat bg-[url('../../../public/IMG_2925.JPG')]">
+    <section className="h-screen w-full bg-cover bg-center bg-no-repeat bg-[url('../../../public/IMG_2925.jpg')]">
       {/* フレックスボックスを使用して親要素を中央揃え */}
       <div className="h-full w-full flex items-start justify-center pt-32">
         <div className="lg:w-1/3 md:w-1/2 max-w-lg bg-[rgb(12,13,40)] rounded-lg p-8 flex flex-col w-full mt-10 md:mt-0 relative z-10 shadow-md">
